ef13608c8b6651390dc52883a8238a0ec06d7c82
Denis-fabian filip <filipdenis12@gmail.com>
2025-11-16 12:13:28 +0100

    Update es02.py

--- lab4/es02/es02.py
+++ lab4/es02/es02.py
@@ -12,19 +12,13 @@ def read_graph_coloring_file(filename):
 
     with open(filename, "r") as f:
         first_line = f.readline().strip().split()
-        num_nodes = int(first_line[0])
-        num_colors = int(first_line[1])
+        num_nodes = int(first_line[0])  # read numbers of nodes
+        num_colors = int(first_line[1])  # read numbers of colours
 
-        for line in f:
+        for line in f:  # read edges
             if line.strip():
                 i, j = map(int, line.split())
                 edges.append((i, j))
-
-    # Detect if nodes are 1-based → convert to 0-based
-    # Se il nodo minimo è 1, allora sottrai 1
-    min_node = min(min(i, j) for i, j in edges)
-    if min_node == 1:
-        edges = [(i - 1, j - 1) for i, j in edges]
 
     return num_nodes, num_colors, edges
 
@@ -41,23 +35,27 @@ print("Archi:", edges)
 
 
 # 2. Crea le variabili x_{i,c}
-# x = {(i, c): boolean_var(f"x_{i}_{c}") for i in range(num_nodi) for c in range(num_colori)}
 
+# each node i we assign c --> 0 or 1
 x = {
-    (i, c): boolean_var(f"x_{i}_{c}")
-    for i in range(num_nodes)
+    (i, c): boolean_var(f"x_{i}_{c}")  # create QUBO variables
+    for i in range(num_nodes)  # node i
     for c in range(num_colors)
-}
-
+}  # colour c
 
 #  3. Inizializza il QUBO
 model = QUBO()
 
 # 4. Vincolo: ogni nodo ha un solo colore
+
+# square penaly is zero only if exactly one color is chosen
 for i in range(num_nodes):
     model += (1 - sum(x[(i, c)] for c in range(num_colors))) ** 2
 
+
 # 5. Vincolo: nodi adiacenti non devono avere lo stesso colore
+
+# if both nodes of an edge have same colour --> penalty added
 for i, j in edges:
     for c in range(num_colors):
         model += x[(i, c)] * x[(j, c)]
@@ -67,7 +65,7 @@ for i, j in edges:
 result = anneal_qubo(model)
 print(result)
 
-# *************************
+# *********************************************
 # ESTRAZIONE DELLA SOLUZIONE DAL PRINT
 # *************************
 
@@ -77,10 +75,10 @@ result_str = str(result)  # converti tutto in stringa
 match = re.search(r"state:\s*(\{.*\})", result_str)
 state_str = match.group(1)  # prende la parte {...}
 
-# Converti la stringa in dizionario Python vero
+# Converti la stringa in dizionario
 solution = ast.literal_eval(state_str)
 
-# *************************
+# *****************************************
 # COSTRUZIONE COLORAZIONE
 # *************************
 
@@ -89,7 +87,7 @@ coloring = {}
 for i in range(num_nodes):
     for c in range(num_colors):
         var_name = f"x_{i}_{c}"
-        if solution[var_name] == 1:
+        if solution[var_name] == 1:  # x_0_1 : 1
             coloring[i] = c
             break
 
@@ -117,7 +115,7 @@ nx.draw(
     node_size=950,
     font_size=16,
     font_weight="bold",
-    edge_color="#555555",
+    edge_color="#555555",  # grigio scuro
 )
 
 plt.title("Colorazione del grafo (Simulated Annealing)", fontsize=18, fontweight="bold")
